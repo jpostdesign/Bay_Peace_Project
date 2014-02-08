9511df66b44394813d4e9e959bf491446fd2b37f
jpostdesign <jpost.design@gmail.com>
2014-02-07 17:26:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Bay_Peace_Project
+=================
+
+web work for non profit client Bay Peace
+
+Initial project scope: a microsite that has a 1-2 min video with information about engaging youth, and by engagement it will greatly benefit society. Additionally, have related article, images, and call to action.
+
+**Feature List**
+
+Contained YouTube motion graphic video
+
+Responsive Design
+
+Centered layout, with max width following good typography
---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php 
+ $to = "emailNameNEEEEDED@doman.com"; 
+ $from = $_REQUEST['Email']; 
+ $name = $_REQUEST['Name'] ; 
+ $headers = "From: $from"; 
+ $subject = "Web Contact Data"; 
+ 
+ $fields = array(); 
+ $fields{"Name"} = "Name"; 
+ $fields{"Email"} = "Email"; 
+ $fields{"list"} = "Mailing List"; 
+ $fields{"Message"} = "Message"; 
+ 
+ $body = "Contact details from the web form:\n\n"; foreach($fields as $a => $b){ 	$body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); } 
+ 
+ $headers2 = "From: baypeace@baypeace.org"; 
+ $subject2 = "Thank you for contacting us"; 
+ $autoreply = "Thank you for contacting us and making a difference! If you have any more questions, please consult our website at http://www.baypeace.org/";
+ 
+ if($from == '') {print "You have not entered an email, please go back and try again";} 
+ else { 
+ if($name == '') {print "You have not entered a name, please go back and try again";} 
+ else { 
+ $send = mail($to, $subject, $body, $headers); 
+ $send2 = mail($from, $subject2, $autoreply, $headers2); 
+ if($send) 
+ {header( "Location: thankyou.html" );} 
+ else 
+ {print "We encountered an error sending your mail, please notify webmaster@YourCompany.com"; } 
+ }
+}
+ ?> 
