7bc253c49d24b26f27e4f77781fe0ae49588d109
jpostdesign <jpost.design@gmail.com>
2014-02-11 19:42:41 -0800

    updated contact form

--- contact.php
+++ contact.php
@@ -1,21 +1,22 @@
 <?php 
- $to = "emailNameNEEEEDED@doman.com"; 
+ $to = "sample@example.com"; 
  $from = $_REQUEST['Email']; 
  $name = $_REQUEST['Name'] ; 
  $headers = "From: $from"; 
- $subject = "Web Contact Data"; 
+ $subject = "Power of Youth Newsletter/Get Involved Contact"; 
  
  $fields = array(); 
  $fields{"Name"} = "Name"; 
  $fields{"Email"} = "Email"; 
  $fields{"list"} = "Mailing List"; 
+ $fields{"Zipcode"} = "Zipcode";
  $fields{"Message"} = "Message"; 
  
  $body = "Contact details from the web form:\n\n"; foreach($fields as $a => $b){ 	$body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); } 
  
- $headers2 = "From: baypeace@baypeace.org"; 
+ $headers2 = "From: sample@example.com"; 
  $subject2 = "Thank you for contacting us"; 
- $autoreply = "Thank you for contacting us and making a difference! If you have any more questions, please consult our website at http://www.baypeace.org/";
+ $autoreply = "Thank you for contacting us and making a difference! If you have any more questions, please consult our main website at http://www.baypeace.org/ for general information and contact information";
  
  if($from == '') {print "You have not entered an email, please go back and try again";} 
  else { 
